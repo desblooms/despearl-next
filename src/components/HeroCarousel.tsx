fc56a0d7aa4728a93db8744be034d298fdb853ce
Shafeeq <desblooms@gmail.com>
2026-09-12 12:24:02 +0530

    Banner Fix

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -55,7 +55,7 @@ export default function HeroCarousel({ banners = [] }: { banners: any[] }) {
                 decoding="async" 
                 sizes="100vw" 
               />
-              <div className="absolute inset-0 bg-transparent"></div>
+              <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-black/30 to-transparent"></div>
               
               <div className="absolute inset-0 flex items-center">
                 <div className="max-w-[1440px] w-full mx-auto px-4 md:px-6">
